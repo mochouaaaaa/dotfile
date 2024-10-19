3a3f3f085a473ec329e7c72e59f99988f213972b
nmc <nn.mochou+github@gmail.com>
2024-10-20 01:50:09 +0800

    delete(script):Remove useless scripts

--- kitty/send_key.py
+++ kitty/send_key.py
@@ -1,3 +1,4 @@
+import re
 from typing import Union
 from abc import ABCMeta, abstractmethod
 
@@ -20,6 +21,13 @@ class BaseCommandKeyMap(metaclass=ABCMeta):
         "]": "right_bracket",  # ']' 替换为 'right_bracket'
         "(": "left_paren",  # '(' 替换为 'left_paren'
         ")": "right_paren",  # ')' 替换为 'right_paren'
+    }
+
+    DIRECTORY_KEY_MAP = {
+        "h": "left",
+        "j": "down",
+        "k": "up",
+        "l": "right",
     }
 
     def __init__(self, boss, window, keymap: str):
@@ -106,6 +114,9 @@ class BaseCommandKeyMap(metaclass=ABCMeta):
 class ZshCommandKeyMap(BaseCommandKeyMap):
     def __init__(self, boos, window, keymap: str):
         super().__init__(boos, window, keymap)
+        self._init_direction(r"^cmd\+ctrl\+([hjkl])$", self._split_window)
+        self._init_direction(r"^ctrl\+shift\+([hjkl])$", self._resize_window)
+        self._init_direction(r"^cmd\+([hjkl])$", self._move_window)
 
     def create_reset_keymap(self) -> list[str]:
         """
@@ -116,6 +127,96 @@ class ZshCommandKeyMap(BaseCommandKeyMap):
     def create_tab_keymap(self):
         for index in range(1, self.tab_num):
             setattr(self, f"cmd_{index}", lambda index=index: self.boss.goto_tab(tab_num=index))
+
+    def _init_direction(self, pattern, func):
+        match = re.match(pattern, self.keymap)
+        if match:
+            func()
+
+    def _resize_window(self):
+        amount = 3  # resize amount
+
+        def _func(boss, window, direction):
+            neighbors = boss.active_tab.current_layout.neighbors_for_window(window, boss.active_tab.windows)
+
+            left_neighbors = neighbors.get("left")
+            right_neighbors = neighbors.get("right")
+            top_neighbors = neighbors.get("top")
+            bottom_neighbors = neighbors.get("bottom")
+
+            # has a neighbor on both sides
+            if direction == "left" and (left_neighbors and right_neighbors):
+                boss.active_tab.resize_window("narrower", amount)
+            # only has left neighbor
+            elif direction == "left" and left_neighbors:
+                boss.active_tab.resize_window("wider", amount)
+            # only has right neighbor
+            elif direction == "left" and right_neighbors:
+                boss.active_tab.resize_window("narrower", amount)
+
+            # has a neighbor on both sides
+            elif direction == "right" and (left_neighbors and right_neighbors):
+                boss.active_tab.resize_window("wider", amount)
+            # only has left neighbor
+            elif direction == "right" and left_neighbors:
+                boss.active_tab.resize_window("narrower", amount)
+            # only has right neighbor
+            elif direction == "right" and right_neighbors:
+                boss.active_tab.resize_window("wider", amount)
+
+            # has a neighbor above and below
+            elif direction == "up" and (top_neighbors and bottom_neighbors):
+                boss.active_tab.resize_window("shorter", amount)
+            # only has top neighbor
+            elif direction == "up" and top_neighbors:
+                boss.active_tab.resize_window("taller", amount)
+            # only has bottom neighbor
+            elif direction == "up" and bottom_neighbors:
+                boss.active_tab.resize_window("shorter", amount)
+
+            # has a neighbor above and below
+            elif direction == "down" and (top_neighbors and bottom_neighbors):
+                boss.active_tab.resize_window("taller", amount)
+            # only has top neighbor
+            elif direction == "down" and top_neighbors:
+                boss.active_tab.resize_window("shorter", amount)
+            # only has bottom neighbor
+            elif direction == "down" and bottom_neighbors:
+                boss.active_tab.resize_window("taller", amount)
+
+        for directory_key, direction in self.DIRECTORY_KEY_MAP.items():
+            setattr(
+                self,
+                f"ctrl_shift_{directory_key}",
+                lambda direction=direction, window=self.window, boss=self.boss: _func(boss, window, direction),
+            )
+
+    def _move_window(self):
+        def _func(direction, boss):
+            boss.active_tab.neighboring_window(direction)
+
+        for directory_key, direction in self.DIRECTORY_KEY_MAP.items():
+            if direction == "up":
+                direction = "top"
+            if direction == "down":
+                direction = "bottom"
+            setattr(self, f"cmd_{directory_key}", lambda direction=direction, boss=self.boss: _func(direction, boss))
+
+    def _split_window(self):
+        def _func(direction, boss):
+            if direction == "up" or direction == "down":
+                boss.launch("--cwd=current", "--location=hsplit")
+            else:
+                boss.launch("--cwd=current", "--location=vsplit")
+
+            if direction == "up" or direction == "left":
+                # boss.active_tab.move_window(direction)
+                boss.active_tab.neighboring_window(direction)
+
+        for directory_key, direction in self.DIRECTORY_KEY_MAP.items():
+            setattr(
+                self, f"cmd_ctrl_{directory_key}", lambda direction=direction, boss=self.boss: _func(direction, boss)
+            )
 
     def cmd_w(self):
         """
@@ -237,6 +338,30 @@ class TmuxCommandKeyMap(ZshCommandKeyMap):
             keymap_list = keymap.split("->")
             for keymap in keymap_list:
                 super().send_keymap(keymap)
+
+    def _split_window(self):
+        for direction in self.DIRECTORY_KEY_MAP.values():
+            setattr(
+                self,
+                f"cmd_ctrl_{direction}",
+                self.default_keymap,
+            )
+
+    def _move_window(self):
+        for direction in self.DIRECTORY_KEY_MAP.values():
+            setattr(
+                self,
+                f"cmd_{direction}",
+                self.default_keymap,
+            )
+
+    def _resize_window(self):
+        for direction in self.DIRECTORY_KEY_MAP.values():
+            setattr(
+                self,
+                f"ctrl_shift_{direction}",
+                self.default_keymap,
+            )
 
     def cmd_t(self):
         self.send_keymap("ctrl+a->c")
