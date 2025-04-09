942146a96db9cf7b26f68d1fcdf9f51cf2c32e80
mochouaaaaa <nn.mochou+github@gmail.com>
2025-04-09 20:30:45 +0800

    delete(zsh): move nix config

--- zsh/plugins/keymap.zsh
+++
@@ -1,4 +0,0 @@
-
-bindkey '^?' backward-delete-char # Ctrl+退格
-bindkey '^h' beginning-of-line    # Ctrl+h 移动到行首
-bindkey '^l' end-of-line        # Ctrl+l 移动到行尾
--- zsh/scripts/packages.sh
+++
@@ -1,4 +0,0 @@
-#/bin/bash
-
-# Install packages
-packages=(git btop fd dust lsd procs ripgrep bat yazi eza less tldr emojify ffmpegthumbnailer chafa jq ouch imagemagick)
