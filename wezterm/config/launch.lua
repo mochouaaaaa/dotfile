aeddcd660a1d22c11be03d0ad88d09291ae79b6e
mochouaaaaa <nn.mochou+github@gmail.com>
2025-05-07 13:32:44 +0800

    fix(wezterm): start shell env

--- wezterm/config/launch.lua
+++ wezterm/config/launch.lua
@@ -8,10 +8,17 @@ local options = {
 -- 判断是否在 tmux 环境中
 local function is_inside_tmux() return os.getenv("TMUX") ~= nil end
 
+local function get_shell()
+	local default_shell = os.getenv("SHELL")
+	if default_shell and string.find(default_shell, "zsh") then
+		return default_shell
+	end
+	return "zsh"
+end
+
 if platform.is_mac then
 	options.default_prog = {
-		"/usr/local/bin/zsh",
-		-- "-l",
+		get_shell(),
 	}
 	options.launch_menu = {}
 end
