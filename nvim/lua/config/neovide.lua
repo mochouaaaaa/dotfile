a18aa48b6af34a4239ca588931372e78b405f1da
mochouaaaaa <nn.mochou+github@gmail.com>
2025-05-11 18:27:23 +0800

    refactor(nvim) neovide config

--- nvim/lua/config/neovide.lua
+++ nvim/lua/config/neovide.lua
@@ -1,6 +1,35 @@
-local M = {}
+local function refresh()
+	vim.g.neovide_refresh_rate = 60
+	vim.g.neovide_refresh_rate_idle = 5
+end
 
-function M.background()
+local function keymap()
+	vim.keymap.set("n", "<D-v>", '"+p', { noremap = true, silent = true }) -- 普通模式下
+	vim.keymap.set("i", "<D-v>", "<C-r>+", { noremap = true, silent = true }) -- 插入模式下
+end
+
+local BaseConfig = {}
+BaseConfig.__index = BaseConfig
+
+function BaseConfig:new()
+	local _self = setmetatable({}, self)
+
+	refresh()
+	keymap()
+
+	_self.window_settings(self)
+	_self.cursor(self)
+	_self.background(self)
+
+	return _self
+end
+
+function BaseConfig:cursor()
+	vim.g.neovide_cursor_trail_size = 1.0
+	vim.g.neovide_cursor_animation_length = 0.04
+end
+
+function BaseConfig:background()
 	vim.g.neovide_floating_blur_amount_x = 2.0
 	vim.g.neovide_floating_blur_amount_y = 2.0
 	vim.g.neovide_floating_shadow = true
@@ -10,7 +39,7 @@ function M.background()
 	vim.g.neovide_normal_opacity = 1
 end
 
-function M.window_settings()
+function BaseConfig:window_settings()
 	vim.g.neovide_padding_top = 0
 	vim.g.neovide_padding_bottom = 0
 	vim.g.neovide_padding_right = 0
@@ -18,32 +47,36 @@ function M.window_settings()
 	vim.g.neovide_remember_window_size = true
 
 	vim.g.neovide_floating_corner_radius = 1
+end
 
+local DarwinConfig = setmetatable({}, { __index = BaseConfig })
+DarwinConfig.__index = DarwinConfig
+
+function DarwinConfig:background()
+	BaseConfig.background(self)
+	vim.g.neovide_opacity = 0.75
+	vim.g.neovide_window_blurred = true
+	-- Helper function for transparency formatting
+	local alpha = function()
+		return string.format("%x", math.floor(255 * vim.g.transparency or 0.8))
+	end
+	-- g:neovide_opacity should be 0 if you want to unify transparency of content and title bar.
+	vim.g.transparency = 0.8
+	vim.g.neovide_background_color = "#1e1e2e" .. alpha()
+end
+
+function DarwinConfig:window_settings()
+	BaseConfig.window_settings(self)
 	vim.g.neovide_window_blurred = true -- mac
 end
 
-function M.refresh()
-	vim.g.neovide_refresh_rate = 60
-	vim.g.neovide_refresh_rate_idle = 5
-end
+local LinuxConfig = setmetatable({}, { __index = BaseConfig })
+LinuxConfig.__index = LinuxConfig
 
-function M.keymap()
-	vim.keymap.set("n", "<D-v>", '"+p', { noremap = true, silent = true }) -- 普通模式下
-	vim.keymap.set("i", "<D-v>", "<C-r>+", { noremap = true, silent = true }) -- 插入模式下
-end
+local platform = require("util.keymap")
 
-function M.config()
-	vim.g.neovide_show_broder = false
-	vim.g.neovide_input_macos_option_key_is_meta = "only_left" -- mac
-	vim.g.neovide_cursor_vfx_mode = "railgun"
+if platform.is_mac() then
+	DarwinConfig:new()
+elseif platform.is_linux() then
+	LinuxConfig:new()
 end
-
-function M.init()
-	M.config()
-	M.keymap()
-	M.background()
-	M.window_settings()
-	M.refresh()
-end
-
-M.init()
