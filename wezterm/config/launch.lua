77c903e757102b0f183f8f41353452340cb9b565
mochouaaaaa <nn.mochou+github@gmail.com>
2025-07-05 21:39:47 +0800

    fix(wezterm):prog shell

--- wezterm/config/launch.lua
+++ wezterm/config/launch.lua
@@ -1,25 +1,26 @@
 local platform = require("utils.platform")()
 
-local options = {
-	default_prog = {},
-	launch_menu = {},
-}
-
 -- 判断是否在 tmux 环境中
 local function is_inside_tmux() return os.getenv("TMUX") ~= nil end
 
 local function get_shell()
 	local default_shell = os.getenv("SHELL")
+	print(default_shell)
 	if default_shell and string.find(default_shell, "zsh") then
 		return default_shell
 	end
 	return "zsh"
 end
 
-if platform.is_mac then
-	options.default_prog = {
+local options = {
+	default_prog = {
 		get_shell(),
-	}
+	},
+	launch_menu = {},
+}
+
+if platform.is_mac then
+	options.default_prog = {}
 	options.launch_menu = {}
 end
 
