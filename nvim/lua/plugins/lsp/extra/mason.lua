d4c30d8cfb6ec90dd799b137823ac79dbb53f51e
mochou <nn.mochou+github@gmail.com>
2025-10-12 05:36:15 +0800

    fix(nvim): use nix disabled mason plugin

--- nvim/lua/plugins/lsp/extra/mason.lua
+++ nvim/lua/plugins/lsp/extra/mason.lua
@@ -1,7 +1,13 @@
 return {
-	"mason-org/mason.nvim",
-	enabled = not vim.g.IS_NIX,
-	opts = {
-		automatic_installation = not vim.g.IS_NIX,
+	{
+		"mason-org/mason.nvim",
+		enabled = not vim.g.IS_NIX,
+		opts = {
+			automatic_installation = not vim.g.IS_NIX,
+		},
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		enabled = not vim.g.IS_NIX,
 	},
 }
