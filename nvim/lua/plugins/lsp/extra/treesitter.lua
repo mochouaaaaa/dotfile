13e0062cd56c7447618cdf055052bae9338abd0a
mochou <nn.mochou+github@gmail.com>
2025-10-20 12:39:33 +0800

    chore: lock versions

--- nvim/lua/plugins/lsp/extra/treesitter.lua
+++ nvim/lua/plugins/lsp/extra/treesitter.lua
@@ -1,26 +1,28 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
-		build = false,
-		opts = {
-			auto_install = not vim.g.IS_NIX,
-			sync_install = not vim.g.IS_NIX,
-			ignore_install = {},
+		branch = "master",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				auto_install = not vim.g.IS_NIX,
+				sync_install = not vim.g.IS_NIX,
+				ignore_install = {},
 
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = false,
-			},
-
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "<CR>",
-					node_incremental = "<CR>",
-					scope_incremental = false,
-					node_decremental = "<S-CR>",
+				highlight = {
+					enable = true,
+					additional_vim_regex_highlighting = false,
 				},
-			},
-		},
+
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						init_selection = "<CR>",
+						node_incremental = "<CR>",
+						scope_incremental = false,
+						node_decremental = "<S-CR>",
+					},
+				},
+			})
+		end,
 	},
 }
