8d5795d73c9aac3fe67b8b9506d7de71604d414b
mochou <nn.mochou+github@gmail.com>
2026-01-01 01:08:43 +0800

    refactor(zsh): remove zsh useless configuration

--- zsh/init.zsh
+++
@@ -1,9 +0,0 @@
-
-PWD_PATH=$HOME/.config/zsh
-PLUGIN_PATH=$PWD_PATH/plugins
-
-for i ($PLUGIN_PATH/*.zsh) {
-    # zinit snippet "$i"
-    source "$i"
-}
-
--- zsh/plugins/docker.zsh
+++
@@ -1,5 +0,0 @@
-
-source $HOME/.config/zsh/utils.zsh
-
-check_command_exist "docker" "alias get_run_command='docker run --rm -v /var/run/docker.sock:/var/run/docker.sock cucker/get_command_4_run_container'"
-
